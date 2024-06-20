f9bb72d56e06b80a6ff13fb5330727efb15419e2
Adrián S <adriansotes@gmail.com>
2024-06-20 16:24:09 +0200

    selectors getMaxPrice, getMinPrice and getIsLogged  true/false tested

--- src/store/__tests__/selectors.test.jsx
+++ src/store/__tests__/selectors.test.jsx
@@ -1,4 +1,26 @@
-import { getAdDetail } from "../selectors";
+import {
+  getAdDetail,
+  getIsLogged,
+  getMaxPrice,
+  getMinPrice,
+} from "../selectors";
+
+describe("getIsLogged selector", () => {
+  it("should return the value of auth when not logged", () => {
+    const state = {
+      auth: false,
+    };
+    const authState = false;
+    expect(getIsLogged(state)).toBe(authState);
+  });
+  it("should return the value of auth when logged", () => {
+    const state = {
+      auth: true,
+    };
+    const authState = true;
+    expect(getIsLogged(state)).toBe(authState);
+  });
+});
 
 describe("getAdDetail selector", () => {
   const state = {
@@ -10,5 +32,25 @@ describe("getAdDetail selector", () => {
     const advertId = "1";
     const expectedResult = { id: "1" };
     expect(getAdDetail(state, advertId)).toEqual(expectedResult);
+  });
+});
+
+describe("getMaxPrice / getMinPrice selectors", () => {
+  const state = {
+    prices: {
+      maxPriceAvailable: 30,
+      minPriceAvailable: 2,
+    },
+  };
+  it("should return the value for maxPriceAvailable", () => {
+    const obtainedMaxPrice = 30;
+
+    expect(getMaxPrice(state)).toBe(obtainedMaxPrice);
+  });
+
+  it("should return the value for minPriceAvailable", () => {
+    const obtainedMinPrice = 2;
+
+    expect(getMinPrice(state)).toBe(obtainedMinPrice);
   });
 });
